470b3539a3037463605ae39d8a69c7caa7d58ad6
wata toshiki <wata.hal.student@gmail.com>
2025-11-14 16:42:14 +0900

    feat: Add Omnisharp LSP support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,7 +19,7 @@ return {
 
             require("mason").setup()
             require("mason-lspconfig").setup({
-                ensure_installed = {},
+                ensure_installed = { "omnisharp" },
             })
 
         end,
